f0278f43b49814cce1cae5feca0f2c95578fd086
Ashuka <ekomlionel@gmail.com>
2024-03-28 22:25:09 +0100

    Django Ajouter un lien vers les détails

--- members/views.py
+++ members/views.py
@@ -8,8 +8,16 @@ from .models import Member
 # Create your views here.
 def members(request):
   mymembers = Member.objects.all().values()
-  template = loader.get_template('all_members.html')
+  template = loader.get_template('myfirst.html')
   context = {
     'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
   }
   return HttpResponse(template.render(context, request))
