2d453a53e86eca959e2db146810fd909f43c0ee0
Ashuka <ekomlionel@gmail.com>
2024-03-29 12:31:57 +0100

    Variables du modèle Django

--- members/views.py
+++ members/views.py
@@ -27,8 +27,12 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
+  # context = {
+  #   'fruits': ['Apple', 'Banana', 'Cherry', 'Ananas', 'Cocktail'],   
+  # }
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry', 'Ananas', 'Cocktail'],   
+    'mymembers': mymembers,
   }
-  return HttpResponse(template.render(context, request)) 
+  return HttpResponse(template.render(context, request))
