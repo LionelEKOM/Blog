5accdb1b82a0b004399f12039ac148e8997500d3
Ashuka <ekomlionel@gmail.com>
2024-03-30 14:15:47 +0100

    include header

--- Blog/settings.py
+++ Blog/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members'
+    'members',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
